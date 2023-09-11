cceedc410e784fab9da85dc766e3717e0a4cc904
jilei-hao <jilei.cs@gmail.com>
2023-09-11 12:52:48 -0400

    user creation page

---
+++ app/user_registration/page.jsx
@@ -0,0 +1,104 @@
+'use client'
+
+import { useState } from 'react';
+
+export default function UserRegistration() {
+  const GatewayURL = process.env.NEXT_PUBLIC_GATEWAY_URL;
+  console.log("GatewayURL: ", GatewayURL);
+
+  // State to store form data
+  const [formData, setFormData] = useState({
+    email: '',
+    password: '',
+    passwordConfirm: ''
+  });
+
+  // Handle form input changes
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
+
+  // Handle form submission
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    console.log("handleSubmit: ", formData);
+
+    if (formData.password !== formData.passwordConfirm) {
+      alert('Passwords do not match.');
+      return;
+    }
+
+    // Make a POST request to your backend API to create a new user
+    try {
+      const response = await fetch(`${GatewayURL}/user`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(formData),
+      });
+
+      console.log("response: ", response);
+
+      if (response.status === 201) {
+        // User created successfully
+        console.log('User created successfully');
+        // Redirect or display a success message
+      } else {
+        // Handle errors, e.g., validation errors from the backend
+        console.error('Error creating user');
+      }
+    } catch (error) {
+      console.error('Error creating user', error);
+    }
+  };
+
+  return (
+    <div>
+      <h1>New User Registration</h1>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="email">Email:</label>
+          <input
+            type="email"
+            id="email"
+            name="email"
+            value={formData.email}
+            onChange={handleInputChange}
+            required
+          />
+        </div>
+        <div>
+          <label htmlFor="pw">Password:</label>
+          <input
+            type="password"
+            id="password"
+            name="password"
+            value={formData.password}
+            onChange={handleInputChange}
+            required
+          />
+        </div>
+        <div>
+          <label htmlFor="passwordConfirm">Confirm Password:</label>
+          <input
+            type="password"
+            id="passwordConfirm"
+            name="passwordConfirm"
+            value={formData.passwordConfirm}
+            onChange={handleInputChange}
+            required
+          />
+        </div>
+        <div>
+          <button type="submit">Create User</button>
+        </div>
+      </form>
+    </div>
+  );
+}
